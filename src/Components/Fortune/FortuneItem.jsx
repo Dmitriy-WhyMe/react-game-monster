3197be558b05e27e3a17f528af2e9550c7551883
Dmitriy <dima.ivahnoff@mail.ru>
2022-05-27 22:48:01 +0300

    katalog-components

---
+++ src/Components/Fortune/FortuneItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const FortuneItem = (props) => {
+  return (
+    <div className="fortune__item" data-aos={props.dataAos} data-aos-easing="linear" data-aos-duration={props.dataAosDuration}>
+        <img className="fortune__img" src={props.mainImageUrl} alt=""/>
+        <img className={props.absolutImageClass} src={props.absolutImageUrl} alt=""/>
+        <div className={props.titleClass}>{props.title}</div>
+        <div className="fortune__desc">
+            <div className="fortune__desc-item">
+                <span>{props.countGame}</span> игр
+            </div>
+            <div className="fortune__desc-item">
+                <span>{props.profitCase}</span> профит
+            </div>
+            <div className="fortune__desc-item">игры от <span>$ {props.startPrice}</span></div>
+        </div>
+        <div className="fortune__discount">-{Math.round((props.priceMain * 100) / props.priceOld)}%</div>
+        <div className="fortune__price">
+            <div className="fortune__price-main">$ {props.priceMain}</div>
+            <div className="fortune__price-old">$ {props.priceOld}</div>
+        </div>
+    </div>
+  )
+}
+
+export default FortuneItem
--- src/Pages/HomePage/Blocks/Fortune.jsx
+++ src/Pages/HomePage/Blocks/Fortune.jsx
@@ -1,70 +1,53 @@
 import React from 'react'
+import FortuneItem from '../../../Components/Fortune/FortuneItem'
 
 function Fortune() {
   return (
     <section className="fortune">
         <h3 className="title-block">Испытай удачу</h3>
         <div className="flex">
-          <div className="fortune__item" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="500">
-            <img className="fortune__img" src="img/fortune/Union.png" alt=""/>
-            <img className="fortune__absolute-1" src="img/fortune/Mask Group (2).png" alt=""/>
-            <div className="fortune__title-1">EPIC</div>
-            <div className="fortune__desc">
-              <div className="fortune__desc-item">
-                <span>800</span> игр
-              </div>
-              <div className="fortune__desc-item">
-                <span>130%</span> профит
-              </div>
-              <div className="fortune__desc-item">игры от <span>$ 900</span>
-              </div>
-            </div>
-            <div className="fortune__discount">- 30%</div>
-            <div className="fortune__price">
-              <div className="fortune__price-main">$ 800</div>
-              <div className="fortune__price-old">$ 800</div>
-            </div>
-          </div>
-          <div className="fortune__item" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="800">
-            <img className="fortune__img" src="img/fortune/Union (1).png" alt=""/>
-            <img className="fortune__absolute-2" src="img/fortune/Mask Group (1).png" alt=""/>
-            <div className="fortune__title-2">COMMON</div>
-            <div className="fortune__desc">
-              <div className="fortune__desc-item">
-                <span>800</span> игр
-              </div>
-              <div className="fortune__desc-item">
-                <span>130%</span> профит
-              </div>
-              <div className="fortune__desc-item">игры от <span>$ 900</span>
-              </div>
-            </div>
-            <div className="fortune__discount">- 30%</div>
-            <div className="fortune__price">
-              <div className="fortune__price-main">$ 800</div>
-              <div className="fortune__price-old">$ 800</div>
-            </div>
-          </div>
-          <div className="fortune__item" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1100">
-            <img className="fortune__img" src="img/fortune/Union (2).png" alt=""/>
-            <img className="fortune__absolute-3" src="img/fortune/Mask Group.png" alt=""/>
-            <div className="fortune__title-3">LEGEND</div>
-            <div className="fortune__desc">
-              <div className="fortune__desc-item">
-                <span>800</span> игр
-              </div>
-              <div className="fortune__desc-item">
-                <span>130%</span> профит
-              </div>
-              <div className="fortune__desc-item">игры от <span>$ 900</span>
-              </div>
-            </div>
-            <div className="fortune__discount">- 30%</div>
-            <div className="fortune__price">
-              <div className="fortune__price-main">$ 800</div>
-              <div className="fortune__price-old">$ 800</div>
-            </div>
-          </div>
+          <FortuneItem 
+            dataAos="zoom-in"
+            dataAosDuration="500"
+            mainImageUrl="img/fortune/Union.png"
+            absolutImageUrl="img/fortune/Mask Group (2).png"
+            absolutImageClass="fortune__absolute-1"
+            title="EPIC"
+            titleClass="fortune__title-1"
+            countGame="800"
+            profitCase="130%"
+            startPrice="900"
+            priceMain="500"
+            priceOld="900"
+          />
+          <FortuneItem 
+            dataAos="zoom-in"
+            dataAosDuration="800"
+            mainImageUrl="img/fortune/Union (1).png"
+            absolutImageUrl="img/fortune/Mask Group (1).png"
+            absolutImageClass="fortune__absolute-2"
+            title="COMMON"
+            titleClass="fortune__title-2"
+            countGame="800"
+            profitCase="130%"
+            startPrice="900"
+            priceMain="400"
+            priceOld="900"
+          />
+          <FortuneItem 
+            dataAos="zoom-in"
+            dataAosDuration="1100"
+            mainImageUrl="img/fortune/Union (2).png"
+            absolutImageUrl="img/fortune/Mask Group.png"
+            absolutImageClass="fortune__absolute-3"
+            title="LEGEND"
+            titleClass="fortune__title-3"
+            countGame="800"
+            profitCase="130%"
+            startPrice="900"
+            priceMain="400"
+            priceOld="1200"
+          />
         </div>
       </section>
   )
